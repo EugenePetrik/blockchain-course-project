595921653d282883425b1c63bd34a27acad7ea0f
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-06-24 15:19:51 +0300

    Use JS for the first task

--- src/task-1-cryptography/cryptography.js
+++ src/task-1-cryptography/cryptography.js
@@ -1,58 +1,47 @@
-import crypto from 'crypto';
-import { logger } from '../utils/logger';
+/* eslint-disable no-console */
+
+const crypto = require('crypto');
 
 /*
   Task 1: Identify the correct symmetric key
-
   Keys in HEX:
     - Key 1: 68544020247570407220244063724074
     - Key 2: 54684020247570407220244063724074
     - Key 3: 54684020247570407220244063727440
-
   SHA-256 hash of the correct key (in HEX):
     - f28fe539655fd6f7275a09b7c3508a3f81573fc42827ce34ddf1ec8d5c2421c3
 */
-
 const keys = [
   '68544020247570407220244063724074',
   '54684020247570407220244063724074',
   '54684020247570407220244063727440',
 ];
-
 const correctHash = 'f28fe539655fd6f7275a09b7c3508a3f81573fc42827ce34ddf1ec8d5c2421c3';
-
 for (const key of keys) {
   const hash = crypto.createHash('sha256').update(Buffer.from(key, 'hex')).digest('hex');
 
-  logger.info(`Key: ${key}`);
-  logger.info(`Hash: ${hash}`);
-  logger.info(`Matches: ${hash === correctHash}\n`);
+  console.log(`Key: ${key}`);
+  console.log(`Hash: ${hash}`);
+  console.log(`Matches: ${hash === correctHash}\n`);
 }
 
 // Answer:
-
 // Key: 68544020247570407220244063724074
 // Hash: 8f7c7aa61c742c533c1911f3bf15a62641f01e8fcaceef252bf26a5f7a53b046
 // Matches: false
-
 // Key: 54684020247570407220244063724074
 // Hash: f28fe539655fd6f7275a09b7c3508a3f81573fc42827ce34ddf1ec8d5c2421c3
 // Matches: true
-
 // Key: 54684020247570407220244063727440
 // Hash: c667de9c047a7481799b16d5c3f3547064f6cb7ac40b2145a02582b1c734c717
 // Matches: false
-
 /*
   Task 2: Decrypt the AES-128 encrypted message
-
   AES encrypted message (in HEX):
     - 876b4e970c3516f333bcf5f16d546a87aaeea5588ead29d213557efc1903997e
-
   CBC initialization vector (in HEX):
     - 656e6372797074696f6e496e74566563
 */
-
 const correctKeyHex = '54684020247570407220244063724074';
 const key = Buffer.from(correctKeyHex, 'hex'); // Convert the key from HEX to a Buffer
 const iv = Buffer.from('656e6372797074696f6e496e74566563', 'hex'); // Convert the IV from HEX to a Buffer
@@ -60,22 +49,19 @@ const encryptedMessage = Buffer.from(
   '876b4e970c3516f333bcf5f16d546a87aaeea5588ead29d213557efc1903997e',
   'hex',
 ); // Convert the encrypted message from HEX to a Buffer
-
 // Create a decipher object using AES-128-CBC mode
 const decipher = crypto.createDecipheriv('aes-128-cbc', key, iv);
 let decrypted = decipher.update(encryptedMessage, 'hex', 'utf8'); // Decrypt the message
 decrypted += decipher.final('utf8'); // Finalize the decryption
 
-logger.info('Decrypted message:', decrypted);
+console.log('Decrypted message:', decrypted);
 
 // Answer:
 
 // Decrypted message: Hello Blockchain!
-
 /*
   Task 3: Generate an asymmetric Elliptic Curve key-pair
 */
-
 // Generate an EC key-pair using the secp256k1 curve
 const { publicKey, privateKey } = crypto.generateKeyPairSync('ec', {
   namedCurve: 'secp256k1', // Curve name
@@ -89,8 +75,8 @@ const { publicKey, privateKey } = crypto.generateKeyPairSync('ec', {
   },
 });
 
-logger.info('Public Key:\n', publicKey);
-logger.info('Private Key:\n', privateKey);
+console.log('Public Key:\n', publicKey);
+console.log('Private Key:\n', privateKey);
 
 // Answer:
 
@@ -99,25 +85,22 @@ logger.info('Private Key:\n', privateKey);
 // MFYwEAYHKoZIzj0CAQYFK4EEAAoDQgAE+IT4RNXtock0hJclOERaUV1zj/gNfAHQ
 // C4t7UVgtmaR7oxJ8BrGRcMsbmDS5sTph1go2NujOXmLXoEBPqi5L7A==
 // -----END PUBLIC KEY-----
-
 // Private Key:
 //  -----BEGIN PRIVATE KEY-----
 // MIGEAgEAMBAGByqGSM49AgEGBSuBBAAKBG0wawIBAQQgaTyj2jnICZ6iDm8LU5K/
 // gKVnil1Y7/JisEnsU97g4kyhRANCAAT4hPhE1e2hyTSElyU4RFpRXXOP+A18AdAL
 // i3tRWC2ZpHujEnwGsZFwyxuYNLmxOmHWCjY26M5eYtegQE+qLkvs
 // -----END PRIVATE KEY-----
-
 /*
   Task 4: Create a digital signature
 */
-
 const sign = crypto.createSign('SHA256');
 sign.update(decrypted);
 sign.end();
 
 const signature = sign.sign(privateKey, 'hex');
 
-logger.info('Digital Signature:\n', signature);
+console.log('Digital Signature:\n', signature);
 
 // Answer:
 
