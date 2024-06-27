ed6602c1c45420a2b6dd4aae347d6b3a523c771f
Eugene Petrik <eugeniy.petrik@gmail.com>
2024-06-27 21:44:05 +0300

    Update Task 3.3

--- src/task-3-ethereum/with.derivation.path.ts
+++ src/task-3-ethereum/with.derivation.path.ts
@@ -1,10 +1,4 @@
-import {
-  ethers,
-  type HDNodeWallet,
-  type JsonRpcProvider,
-  type Mnemonic,
-  type Wallet,
-} from 'ethers';
+import { ethers, type HDNodeWallet, type JsonRpcProvider, type Mnemonic } from 'ethers';
 import * as dotenv from 'dotenv';
 import { logger } from '../utils/logger';
 
@@ -18,25 +12,26 @@ const provider: JsonRpcProvider = new ethers.JsonRpcProvider(
   `https://sepolia.infura.io/v3/${infuraProjectId}`,
 );
 
-// Create a Mnemonic object
-const mnemonic: Mnemonic = ethers.Mnemonic.fromPhrase(mnemonicPhrase);
-
-// Create an HDNode from the mnemonic
-const hdNode: HDNodeWallet = ethers.HDNodeWallet.fromMnemonic(mnemonic);
-
 const getBalance = async (index: number): Promise<void> => {
-  // Derive the path for the account (without the "m/" prefix)
-  const walletPath: string = `m/44'/60'/0'/0/${index}`;
-  const walletNode: HDNodeWallet = hdNode.derivePath(walletPath.replace(/^m\//, ''));
+  const walletPath: string = `44'/60'/0'/0/${index}`;
 
-  // Create a wallet instance from the derived private key and connect to the provider
-  const wallet: Wallet = new ethers.Wallet(walletNode.privateKey, provider);
+  // Create a Mnemonic object
+  const mnemonic: Mnemonic = ethers.Mnemonic.fromPhrase(mnemonicPhrase);
 
-  const balance: bigint = await provider.getBalance(wallet.address);
+  // Create an HDNode from the mnemonic
+  const hdNodeWallet: HDNodeWallet = ethers.HDNodeWallet.fromMnemonic(
+    mnemonic,
+    walletPath,
+  ).connect(provider);
+
+  const address: string = hdNodeWallet.address;
+  logger.info(`Wallet address is ${address}`);
+
+  const balance: bigint = await provider.getBalance(address);
   logger.info(`Balance of account ${index}: ${ethers.formatEther(balance)} ETH`);
 };
 
 (async () => {
-  await getBalance(0);
-  await getBalance(1);
+  await getBalance(2);
+  await getBalance(3);
 })();
